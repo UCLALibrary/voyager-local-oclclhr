b671a2a2d911d94c40dacc7b820f82c0ae989a23
Andy Kohler <akohler@library.ucla.edu>
2016-08-09 09:13:56 -0700

    Rewrite ZAS LHR sql to force index use for performance, post-migration

--- zas/zas_lhr_serials.sql
+++ zas/zas_lhr_serials.sql
@@ -17,27 +17,26 @@ with updates as (
   where mh.action_date >= to_date('&last_date', 'YYYYMMDD')
   and mh.operator_id != 'nomelvyl'
 )
-select
+select /*+ index(bi BIB_INDEX_BIB_ID_IDX) */
     bm.mfhd_id
  || chr(9)
- || ( select replace(normal_heading, 'UCOCLC', '')
-    from bib_index
-    where bib_id = bm.bib_id
-    and index_code = '0350'
-    and normal_heading like 'UCOCLC%'
-    and rownum < 2
-  ) as oclc
+ || replace(bi.normal_heading, 'UCOCLC', '')
+  as oclc
 from updates u
 inner join bib_text bt on u.bib_id = bt.bib_id
 inner join bib_master br on bt.bib_id = br.bib_id
 inner join bib_mfhd bm on br.bib_id = bm.bib_id
 inner join mfhd_master mm on bm.mfhd_id = mm.mfhd_id
 inner join location l on mm.location_id = l.location_id
+inner join bib_index bi on u.bib_id = bi.bib_id
+  and bi.index_code = '0350'
+  and bi.normal_heading like 'UCOCLC%'
 where (l.location_code = 'sr' or l.location_code like 'srucl%')
 and mm.suppress_in_opac = 'N'
 and br.suppress_in_opac = 'N'
 and bt.bib_format like '%s'
 -- Must have OCLC number
+/*
 and exists (
   select *
   from bib_index
@@ -45,6 +44,7 @@ and exists (
   and index_code = '0350'
   and normal_heading like 'UCOCLC%'
 )
+*/
 -- MFHD must have items - hundreds don't
 and exists (
   select *
